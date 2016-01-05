c31dbc2791989ac14941a4b4c85505cbba98f598
Oscar Swanros <oscar@swanros.com>
2016-01-04 18:04:10 -0600

    Initial commit with gitignore

---
+++ FunnelKit/FunnelKit.h
@@ -0,0 +1,19 @@
+//
+//  FunnelKit.h
+//  FunnelKit
+//
+//  Created by Swanros on 1/4/16.
+//  Copyright © 2016 Reserbus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FunnelKit.
+FOUNDATION_EXPORT double FunnelKitVersionNumber;
+
+//! Project version string for FunnelKit.
+FOUNDATION_EXPORT const unsigned char FunnelKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunnelKit/PublicHeader.h>
+
+
