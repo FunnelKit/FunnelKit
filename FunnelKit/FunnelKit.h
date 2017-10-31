59d63a10ffc2526e65d4d746793aac00b4943a31
Oscar Swanros <oscar@swanros.com>
2017-10-31 14:48:07 -0700

    Restart

---
+++ FunnelKit/FunnelKit.h
@@ -0,0 +1,19 @@
+//
+//  FunnelKit.h
+//  FunnelKit
+//
+//  Created by Oscar Swanros on 10/31/17.
+//  Copyright © 2017 FunnelKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FunnelKit.
+FOUNDATION_EXPORT double FunnelKitVersionNumber;
+
+//! Project version string for FunnelKit.
+FOUNDATION_EXPORT const unsigned char FunnelKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunnelKit/PublicHeader.h>
+
+
