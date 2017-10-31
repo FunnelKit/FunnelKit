256c18805000616af8c0e2cfe7bc8c778c1b255b
Oscar Swanros <oscar@swanros.com>
2017-10-31 14:46:23 -0700

    Clean up

--- FunnelKit/Protocols/FunnelDelegate.swift
+++
@@ -1,6 +0,0 @@
-
-public protocol FunnelDelegate {
-    func funnel(funnel: Funnel, didStartStep step: FunnelStep)
-    func funnel(funnel: Funnel, didCompleteStep step: FunnelStep)
-    func funnel(funnel: Funnel, didCompleteWithCoordinatorOutput output: FunnelCompletionOutput?, errors: [NSError])
-}
